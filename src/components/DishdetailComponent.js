b8aad8e0dd84ef0595dad375374f9073e6d9449e
Joyce Cho <joyce.cho@colorado.edu>
2018-11-27 15:31:47 -0700

    Fetch from Server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -3,6 +3,7 @@ import { Card, CardImg, CardText, CardBody, CardTitle , Breadcrumb, BreadcrumbIt
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from "./LoadingComponent";
+import { baseUrl } from '../shared/baseUrl';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -122,7 +123,7 @@ const minLength = (len) => (val) => val && (val.length >= len);
     function RenderDish({dish}) {
         return (
             <Card>
-                <CardImg top src={dish.image} alt={dish.name}/>
+                <CardImg top src={baseUrl + dish.image} alt={dish.name}/>
                 <CardBody>
                     <CardTitle>{dish.name}</CardTitle>
                     <CardText>{dish.description}</CardText>
